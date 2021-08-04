a46fc420403b4295bd02e6f99258ea58b0cf4510
Rafael Amed Valenzuela <69322700+rafamedtech@users.noreply.github.com>
2021-08-04 15:39:25 -0700

    first commit

---
+++ projects/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+def projects(request):
+    return HttpResponse('Here are our projects')
+
+def project(request, pk):
+    return HttpResponse('Single project' + ' ' + str(pk))
