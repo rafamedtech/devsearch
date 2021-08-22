6d5005327dc1851ed655c89e3f1077a75cd5e5c1
Rafael Amed Valenzuela <69322700+rafamedtech@users.noreply.github.com>
2021-08-22 16:56:16 -0700

    welcome email functionality added

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -121,6 +121,14 @@ USE_L10N = True
 
 USE_TZ = True
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'rafa.ravg@gmail.com'
+EMAIL_HOST_PASSWORD = 'ultobyhkohkiasfl'
+
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
