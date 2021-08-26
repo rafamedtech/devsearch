df243f019a6915887798fd2a1e98e9b3b761b968
Rafael Amed Valenzuela <69322700+rafamedtech@users.noreply.github.com>
2021-08-26 15:36:50 -0700

    rest framework started and vote

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,17 @@
-from api.views import getRoutes
 from django.urls import path
-from django.urls.resolvers import URLPattern
 from . import views
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 urlpatterns = [
-    path('', views.getRoutes)
+    path('users/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('users/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    path('', views.getRoutes),
+    path('projects/', views.getProjects),
+    path('projects/<str:pk>/', views.getProject),
+    path('projects/<str:pk>/vote/', views.projectVote)
 ]
